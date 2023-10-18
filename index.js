2c49b4bf0599708dd25b48194fc39d8f931285de
yvzbly <yavuzbalay90@gmail.com>
2023-10-18 16:45:23 +0200

    1st iteration done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+const hacker1 = 'Yavuz Balay';
+console.log(`The driver's name ${hacker1}`);
+const hacker2 = 'Elise Jonkers';
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
