5bacebbbb387c8a7a7ed4b0a30e1d1f7b8c2ab84
“elisejonkers” <elisejonkers18@hotmail.com>
2023-10-18 17:49:28 +0200

    3rd iteration complete

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ if(hacker2Length > hacker1Length){
 }
 
 // Iteration 3: Loops
+//3.1
 let space = ' ';
 const hacker1Capital = hacker1.toUpperCase();
 let hackernameWithSpaces = ' ';
@@ -29,6 +30,7 @@ for (let i = 0; i < hacker1Capital.length; i++) {
 }
 console.log(hackernameWithSpaces);
 
+//3.2
 let hacker2Reversed = ' ';
 
 for (let i = hacker2.length - 1; i >= 0; i--) {
@@ -37,3 +39,17 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 }
 
 console.log(hacker2Reversed);
+
+//3.3
+// Elise is the driver for this iteration
+const compareValue = hacker1.localeCompare(hacker2)
+console.log(compareValue)
+
+if (compareValue === 1){
+  console.log("The driver's name goes first.")
+} else if (compareValue === -1){
+  console.log("Yo, the navigator goes first, definitely.")
+} else if (compareValue === 0){
+  console.log("What?! You both have the same name?")
+}
+
